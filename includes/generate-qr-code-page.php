d6f9dbaf65a4fb03a73d0bc5d3c0dde1f173eb0b
ali rahimi <alirahimi818@gmail.com>
2023-01-11 17:07:25 +0100

    qr generate bug fixed

--- includes/generate-qr-code-page.php
+++ includes/generate-qr-code-page.php
@@ -1,5 +1,5 @@
 <?php
-include(plugin_dir_path(PLUGIN_FILE_URL) . '/includes/phpqrcode/qrlib.php');
+include(plugin_dir_path(QRVC_PLUGIN_FILE_URL) . '/includes/phpqrcode/qrlib.php');
 $url = site_url('/qr-voucher-show/?code=' . $_GET['string']);
 return QRcode::png($url);
 ?>
